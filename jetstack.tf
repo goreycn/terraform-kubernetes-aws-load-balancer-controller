7e1c572d0fdf2b9897be216ae146018cfe115986
james-bailey <james.bailey@fisglobal.com>
2022-01-20 19:32:11 +0000

    fix unpinnedmodule

--- jetstack.tf
+++ jetstack.tf
@@ -1,4 +1,3 @@
 module "jetstack_certmanager" {
-  source  = "github.com/bailey84j/terraform-kubernetes-jetstack-certmanager"
-  version = "1.0.0"
+  source  = "github.com/bailey84j/terraform-kubernetes-jetstack-certmanager?ref=v1.0.0"
 }
